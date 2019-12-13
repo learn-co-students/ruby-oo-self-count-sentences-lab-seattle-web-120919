eda41a900e91ae7efa1b45d57fbd5efccced6ea1
kkirby16 <kkirby16@my.whitworth.edu>
2019-12-13 08:45:27 -0800

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
-  end
+    self.end_with?(".")
+  
+  end 
 
   def question?
+    self.end_with?("?")
 
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
-  def count_sentences
-
+  def count_sentences #use self keyword 
+    #binding.pry
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+  
   end
-end
+end 
